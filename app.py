263485c0e7a16a81d0348cbcbda15457d82ce1bd
Kate Banner <katebanner@MacBookAir.mynet>
2025-10-12 14:39:14 +0100

    week 3

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ def vote_page():
     st.markdown("---")
 
     # Example: filter questions by topic and type
-    topic_filter = {"topics": "Methods of Development I: Program Evaluation and RCTs"}
+    topic_filter = {"topics": "Methods of Development II: Theory and Data"}
     topic_questions = list(exam_col.find(topic_filter))
 
     if "has_voted" not in st.session_state:
